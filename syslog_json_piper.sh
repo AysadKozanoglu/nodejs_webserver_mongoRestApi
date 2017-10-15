21f8683912b504f6245bb5a5700976f82eec4312
Aysad Kozanoglu <aysadx@gmail.com>
2017-10-16 00:56:56 +0200

    Update syslog_json_piper.sh

--- syslog_json_piper.sh
+++ syslog_json_piper.sh
@@ -5,4 +5,4 @@
 # web: http://aysad.pe.hu
 #
 
-tail -f  /var/log/kern.log | while read line; do  t=$(date +"%s"); read a1 a2 a3 a4 <<< $(echo $line | awk '{print $1 " " $2 " " $3 " " $4}'); a=$(jo date=$a1" "$a2" "$a3 host=$a4  mess="$line"); req=$(echo $a|base64); wget -qO- http://127.0.0.1:8084/add/"${req}"; done;
+tail -f  /var/log/kern.log | while read line; do  t=$(date +"%s"); read a1 a2 a3 a4 <<< $(echo $line | awk '{print $1 " " $2 " " $3 " " $4}'); a=$(jo date=$a1" "$a2" "$a3 host=$a4  mess="$line"); req=$(echo $a|base64); wget -qO- http://127.0.0.1:8080/add/"${req}"; done;
